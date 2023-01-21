cda4d226ecef0b9eb9c79050098b18a94e72f5ec
nevalions <nevalions@ya.ru>
2023-01-21 15:04:04 +0300

    TODO removed

--- services/message_print.py
+++ services/message_print.py
@@ -19,20 +19,3 @@ def print_number_of_rounds_full(season_year, rounds_in_season_year):
     print(f'Final Four Semifinal: Round #{rounds_in_season_year - 1}')
     print(f'Final Four Final: Round #{rounds_in_season_year}')
     print(f"DON'T select FAKE rounds {fake_rounds[0]}, {fake_rounds[1]}, {fake_rounds[2]}, {fake_rounds[3]}")
-
-
-#TODO async await reply from file
-# async def bot_print_number_of_rounds_full(season_year, rounds_in_season_year):
-#     fake_rounds = [
-#         rounds_in_season_year - 6,
-#         rounds_in_season_year - 5,
-#         rounds_in_season_year - 4,
-#         rounds_in_season_year - 3,
-#     ]
-#
-#     await message.reply(
-#         f'Max number of rounds in Regular Season {season_year} - {rounds_in_season_year - 7}\n'
-#         f'Playoffs Best of Five: Round #{rounds_in_season_year - 2}\n'
-#         f'Final Four Semifinal: Round #{rounds_in_season_year - 1}\n'
-#         f'Final Four Final: Round #{rounds_in_season_year}\n'
-#         f"DON'T select FAKE rounds {fake_rounds[0]}, {fake_rounds[1]}, {fake_rounds[2]}, {fake_rounds[3]}")
